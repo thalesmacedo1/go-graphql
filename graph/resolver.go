58362600652fd62124223824fd6b5338fa398ba2
Thales Macêdo <thalesmacedo1@live.com>
2022-08-05 15:47:43 -0300

    implemented query functions

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,9 +1,15 @@
 package graph
 
+import "github.com/thalesmacedo1/go-graphql/graph/model"
+
 //go:generate go run github.com/99designs/gqlgen generate
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Categories []*model.Category
+	Courses    []*model.Course
+	Chapters   []*model.Chapter
+}
